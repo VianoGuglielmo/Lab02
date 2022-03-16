2d4347f3d5d8be893af465568e228bf37b18a2aa
VianoGuglielmo <100919429+VianoGuglielmo@users.noreply.github.com>
2022-03-16 18:54:39 +0100

    inizio

--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -6,7 +6,10 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
@@ -16,8 +19,26 @@ public class FXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="txtDizionario"
+    private TextArea txtDizionario; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtParola"
+    private TextField txtParola; // Value injected by FXMLLoader
+
+    @FXML
+    void doClear(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void doTranslate(ActionEvent event) {
+
+    }
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert txtDizionario != null : "fx:id=\"txtDizionario\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 
