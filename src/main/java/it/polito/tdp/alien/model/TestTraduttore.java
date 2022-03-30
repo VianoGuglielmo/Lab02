e24033c87100de29df0395f888a24526dcfc918d
VianoGuglielmo <100919429+VianoGuglielmo@users.noreply.github.com>
2022-03-30 13:11:48 +0200

    .

--- src/main/java/it/polito/tdp/alien/model/TestTraduttore.java
+++ src/main/java/it/polito/tdp/alien/model/TestTraduttore.java
@@ -2,4 +2,13 @@ package it.polito.tdp.alien.model;
 
 public class TestTraduttore {
 	
+	public static void main (String[] args) {
+		Traduttore tr = new Traduttore();
+		tr.add("cane");
+		tr.add("oane");
+		tr.add("porcodio");
+		
+		System.out.println(tr);
+	}
+	
 }
--- src/main/java/it/polito/tdp/alien/model/Traduttore.java
+++ src/main/java/it/polito/tdp/alien/model/Traduttore.java
@@ -1,5 +1,17 @@
 package it.polito.tdp.alien.model;
 
-public class Traduttore {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Traduttore {
+	private List<String> par;
+	
+	public Traduttore(){
+		this.par = new ArrayList<String>();
+	}
+	
+	public void add(String p) {
+		par.add(p);
+	}
+	
 }
